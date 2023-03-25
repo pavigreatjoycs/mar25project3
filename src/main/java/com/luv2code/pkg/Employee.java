8c12faa86d3afff00b6cb7960c37d3c33db4d0f1
Great Joycs <pavigreatjoycs@gmail.com>
2023-03-25 11:51:48 +0530

    Added getters setters employee

--- src/main/java/com/luv2code/pkg/Employee.java
+++ src/main/java/com/luv2code/pkg/Employee.java
@@ -1,4 +1,19 @@
+package com.luv2code.pkg;
 public class Employee{
   private int employeeId;
   private String employeeName;
+public int getEmployeeId() {
+	return employeeId;
+}
+public void setEmployeeId(int employeeId) {
+	this.employeeId = employeeId;
+}
+public String getEmployeeName() {
+	return employeeName;
+}
+public void setEmployeeName(String employeeName) {
+	this.employeeName = employeeName;
+}
+  
+  
 }
